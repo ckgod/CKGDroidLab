4051487b4de24072a561d2fcbe515ab19206f099
chako <chako@ebay.com>
2024-09-30 15:44:31 +0900

    common, model 모듈 생성

---
+++ core/model/src/main/java/app/ckg/androidlab/core/model/Model.kt
@@ -0,0 +1,4 @@
+package app.ckg.androidlab.core.model
+
+class Model {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "CKGDroidLab"
 include(":app")
- 
+include(":core:common")
+include(":core:model")
